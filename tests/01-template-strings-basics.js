6b5c343950881145c53b611c50136500de0bed88
jos <mail@jos.tw>
2015-06-29 16:19:18 +0800

    add 01-template-strings-basics.js

---
+++ tests/01-template-strings-basics.js
@@ -0,0 +1,27 @@
+// 1: template strings - basics
+// To do: make all tests pass
+
+var assert = require("chai").assert;
+var jsdom = require("jsdom").jsdom;
+
+var document = jsdom();
+
+describe('template string, are wrapped in backticks', function() {
+
+  it('prints x into a string using ${x}', function() {
+    var x = 42;
+    assert.equal(`x=${x}`, 'x=' + x);
+  });
+
+  it('add two numbers inside the ${...}', function() {
+    var x = 42;
+    var y = 23;
+    assert.equal(`${ x } + $ { y }`, x+y);
+  });
+
+  it('get a function call result inside ${...}', function() {
+    function getDomain(){ return document.domain }
+    assert.equal(`${ getDomain }`, '');
+  });
+
+});
