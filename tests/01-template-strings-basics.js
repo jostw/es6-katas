21922d6489b678a33b3017d1f987c150c3bb4737
jos <mail@jos.tw>
2015-06-29 16:34:07 +0800

    fix 01-template-strings-basics.js

--- tests/01-template-strings-basics.js
+++ tests/01-template-strings-basics.js
@@ -16,12 +16,12 @@ describe('template string, are wrapped in backticks', function() {
   it('add two numbers inside the ${...}', function() {
     var x = 42;
     var y = 23;
-    assert.equal(`${ x } + $ { y }`, x+y);
+    assert.equal(`${ x + y }`, x+y);
   });
 
   it('get a function call result inside ${...}', function() {
     function getDomain(){ return document.domain }
-    assert.equal(`${ getDomain }`, '');
+    assert.equal(`${ getDomain() }`, '');
   });
 
 });
