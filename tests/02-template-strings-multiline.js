574f09ae4f59d1a571b69cf0f1737d7bcccfce2e
jos <mail@jos.tw>
2015-06-30 09:56:28 +0800

    add 02-template-strings-multiline.js

---
+++ tests/02-template-strings-multiline.js
@@ -0,0 +1,22 @@
+// 2: template strings - multiline
+// To do: make all tests pass
+
+var assert = require("chai").assert;
+
+describe('template string, can be multiline', function() {
+
+  it('count the lines properly', function() {
+    var multiline = `line 1
+    line 2
+    line 3`;
+    assert.equal(multiline.split('\n').length, 4);
+  });
+
+  it('with variable place holders and spaces matter', function() {
+    var x = 42;
+    var multiline = `line 1
+        ${x}`;
+    assert.equal(multiline, 'line 1\n42');
+  });
+
+});
