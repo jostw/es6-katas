3c5a3e9dd8882bd8d3cab286794e3c4e0e235779
jos <mail@jos.tw>
2015-07-05 05:38:17 +0800

    fix 07-block-scope-let.js

--- tests/07-block-scope-let.js
+++ tests/07-block-scope-let.js
@@ -9,14 +9,14 @@ describe('`let` restricts the scope of the variable to the current block', () =>
 
     it('`var` works as usual', () => {
       if (true) {
-        let varX = true;
+        var varX = true;
       }
       assert.equal(varX, true);
     });
 
     it('`let` restricts scope to inside the block', () => {
       if (true) {
-        var letX = true;
+        let letX = true;
       }
       assert.throws(() => console.log(letX));
     });
@@ -27,13 +27,13 @@ describe('`let` restricts the scope of the variable to the current block', () =>
 
     it('`let` use in `for` loops', () => {
       let obj = {x: 1};
-      for (var key in obj) {}
+      for (let key in obj) {}
       assert.throws(() => console.log(key));
     });
 
     it('create artifical scope, using curly braces', () => {
       {
-        var letX = true;
+        let letX = true;
       }
       assert.throws(() => console.log(letX));
     });
