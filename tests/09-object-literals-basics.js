ab8d8f57a978c752c37790b29db74a92937b675c
jos <mail@jos.tw>
2015-07-07 11:51:41 +0800

    add 09-object-literals-basics.js

---
+++ tests/09-object-literals-basics.js
@@ -0,0 +1,45 @@
+// 9: object-literals - basics
+// To do: make all tests pass, leave the assert lines unchanged!
+
+var assert = require("assert");
+
+describe('new shorthands for objects', () => {
+
+  const x = 1;
+  const y = 2;
+
+  describe('with variables', () => {
+    it('use the variables name as key', () => {
+      const short = {x};
+      assert.deepEqual(short, {y: y});
+    });
+    it('works with many too', () => {
+      const short = {x, y: z};
+      assert.deepEqual(short, {x: x, y: y});
+    });
+  });
+
+  describe('with methods', () => {
+
+    const func = () => func;
+
+    it('uses its name', () => {
+      const short = {it};
+      assert.deepEqual(short, {func: func});
+    });
+
+    it('different key must be given explicitly, just like before ES6', () => {
+      const short = {func};
+      assert.deepEqual(short, {otherKey: func});
+    });
+
+    it('inline function, no need for `function(){}`', () => {
+      const short = {
+        inlineFunc: 'I am inline'
+      };
+      assert.deepEqual(short.inlineFunc(), 'I am inline');
+    });
+  });
+
+});
+
