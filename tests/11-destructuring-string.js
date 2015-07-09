0a6691c3ae0aff491db9039cc674aff804cdf0a5
jos <mail@jos.tw>
2015-07-09 12:50:02 +0800

    fix 11-destructuring-string.js

--- tests/11-destructuring-string.js
+++ tests/11-destructuring-string.js
@@ -7,17 +7,17 @@ describe('destructuring also works on strings', () => {
 
 
   it('destructure every character', () => {
-    let a, b, c = 'abc';
+    let [a, b, c] = 'abc';
     assert.deepEqual([a, b, c], ['a', 'b', 'c']);
   });
 
   it('missing characters are undefined', () => {
-    const [a, c] = 'ab';
+    const [a, b, c] = 'ab';
     assert.equal(c, void 0);
   });
 
   it('unicode character work too', () => {
-    const [space, coffee] = 'a ☕';
+    const [a, space, coffee] = 'a ☕';
     assert.equal(coffee, '\u{2615}');
   });
 
