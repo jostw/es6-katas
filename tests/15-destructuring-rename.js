da32d87bca93a41363bdf8e796ed3f81b8607a60
jos <mail@jos.tw>
2015-07-13 12:07:41 +0800

    fix 15-destructuring-rename.js

--- tests/15-destructuring-rename.js
+++ tests/15-destructuring-rename.js
@@ -6,24 +6,24 @@ var assert = require("assert");
 describe('rename variables while destructuring', () => {
 
   it('rename object key', () => {
-    const {x: newName} = {x: 1};
+    const {x: y} = {x: 1};
     assert.equal(y, 1);
   });
 
   it('object key rename with default value', () => {
-    const {x: y=2} = {y: 23};
+    const {x: y=42} = {y: 23};
     assert.equal(y, 42);
   });
 
   it('rename param in a function param', () => {
-    const fn = ({x}) => {
+    const fn = ({x: y}) => {
       assert.equal(y, 1);
     };
     fn({x: 1});
   });
 
   it('rename param in a function param with default value', () => {
-    const fn = ({x: z=3}) => {
+    const fn = ({x: y=3}) => {
       assert.equal(y, 3);
     };
     fn({});
