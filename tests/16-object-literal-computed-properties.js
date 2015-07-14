47e148e493ca7e3c9ee89d5f7387bc48277f89bb
jos <mail@jos.tw>
2015-07-14 10:58:40 +0800

    fix 16-object-literal-computed-properties.js

--- tests/16-object-literal-computed-properties.js
+++ tests/16-object-literal-computed-properties.js
@@ -7,31 +7,31 @@ describe('object literal properties may be computed values', () => {
 
   it('dynamic property `x`', () => {
     const propertyName = 'x';
-    const obj = {[propertyName]: 2};
+    const obj = {[propertyName]: 1};
     assert.equal(obj.x, 1);
   });
 
   it('computed property `x` as a function', () => {
     const key = 'func';
-    const obj = {[key]: 'seven'};
+    const obj = {[key]() {return 'seven'}};
     assert.equal(obj.func(), 'seven');
   });
 
   it('computed property name results from a function call', () => {
     const getName = () => 'propertyName';
-    const obj = {[getName]() {return 'seven'}};
+    const obj = {[getName()]() {return 'seven'}};
     assert.equal(obj.propertyName(), 'seven');
   });
 
   it('an expression makes up the property name', () => {
-    const what = 'Key';
+    const what = 'tyName';
     const obj = {['proper' + what]: null};
     assert.equal('propertyName' in obj, true);
   });
 
   it('accessor keys can be computed names too', () => {
     const obj = {
-      set ['key'](_) {return 1}
+      get ['key']() {return 1}
     };
     assert.equal(obj.key, 1);
   });
