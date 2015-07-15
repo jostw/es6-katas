a6f2a4c62d6ee8566dd17afe230529ee05b07812
jos <mail@jos.tw>
2015-07-15 14:27:24 +0800

    fix 17-unicode-in-strings.js

--- tests/17-unicode-in-strings.js
+++ tests/17-unicode-in-strings.js
@@ -6,22 +6,22 @@ var assert = require("assert");
 describe('unicode strings', () => {
 
   it('are \\u prefixed', () => {
-    const nuclear = 2622;
+    const nuclear = "\u2622";
     assert.equal(nuclear, '☢');
   });
 
   it('value is 4 bytes/digits', () => {
-    const nuclear = '\u26222';
+    const nuclear = '\u2622';
     assert.equal(`no more ${nuclear}`, 'no more ☢');
   });
 
   it('value is hexadecimal', () => {
-    const nuclear = `\u006B\u006A more \u2622`;
+    const nuclear = `\u006E\u006F more \u2622`;
     assert.equal(nuclear, 'no more ☢');
   });
 
   it('curly braces may surround the value', () => {
-    const nuclear = `\u{0000000006E}\u00006F more \u2622`;
+    const nuclear = `\u{006E}\u006F more \u2622`;
     assert.equal(nuclear, 'no more ☢');
   });
 
