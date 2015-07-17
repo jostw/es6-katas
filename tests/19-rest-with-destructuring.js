32110bf1941cc6a9c267fdf81fb61ae1987cb321
jos <mail@jos.tw>
2015-07-17 21:06:36 +0800

    fix 19-rest-with-destructuring.js

--- tests/19-rest-with-destructuring.js
+++ tests/19-rest-with-destructuring.js
@@ -6,25 +6,25 @@ var assert = require("assert");
 describe('rest with destrcturing', () => {
 
   it('rest parameter must be last', () => {
-    const [...all, last] = [1, 2, 3, 4];
+    const [...all] = [1, 2, 3, 4];
     assert.deepEqual(all, [1, 2, 3, 4]);
   });
 
   it('assign rest of an array to a variable', () => {
-    const [...all] = [1, 2, 3, 4];
+    const [, ...all] = [1, 2, 3, 4];
     assert.deepEqual(all, [2, 3, 4]);
   });
 
   // the following are actually using `spread` ... oops, to be fixed
   it('concat differently', () => {
     const theEnd = [3, 4];
-    const allInOne = [1, 2, ...[theEnd]];
+    const allInOne = [1, 2, ...theEnd];
     assert.deepEqual(allInOne, [1, 2, 3, 4]);
   });
 
   it('`apply` made simple, even for constructors', () => {
     const theDate = [2015, 1, 1];
-    const date = new Date(theDate);
+    const date = new Date(...theDate);
     assert.deepEqual(new Date(2015, 1, 1), date);
   });
 
