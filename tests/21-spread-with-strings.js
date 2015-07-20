f6bf566819927fb9541cbc758f473a4a602c0dfa
jos <mail@jos.tw>
2015-07-20 16:32:15 +0800

    fix 21-spread-with-strings.js

--- tests/21-spread-with-strings.js
+++ tests/21-spread-with-strings.js
@@ -6,29 +6,29 @@ var assert = require("assert");
 describe('spread with strings', () => {
 
   it('simply spread each char of a string', function() {
-    const [b, a] = [...'ab'];
+    const [a, b] = [...'ab'];
     assert.equal(a, 'a');
     assert.equal(b, 'b');
   });
 
   it('extracts each array item', function() {
-    const [a,,c] = ['a', ...'12'];
+    const [,a,b] = ['a', ...'12'];
     assert.equal(a, 1);
     assert.equal(b, 2);
   });
 
   it('works anywhere inside an array (must not be last)', function() {
-    const letters = ['a', 'bcd', 'e', 'f'];
+    const letters = ['a', ...'bcd', 'e', 'f'];
     assert.equal(letters.length, 6);
   });
 
   it('dont confuse with the rest operator', function() {
-    const [...rest] = ['1234', ...'5'];
+    const [...rest] = [...'1234', ...'5'];
     assert.deepEqual(rest, [1, 2, 3, 4, 5]);
   });
 
   it('passed as function parameter', function() {
-    const max = Math.max(12345);
+    const max = Math.max(...'12345');
     assert.deepEqual(max, 5);
   });
 
