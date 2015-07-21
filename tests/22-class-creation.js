f1df1f4eedca6c2afd46840c5ed8461ecd008711
jos <mail@jos.tw>
2015-07-21 12:47:05 +0800

    fix 22-class-creation.js

--- tests/22-class-creation.js
+++ tests/22-class-creation.js
@@ -6,21 +6,20 @@ var assert = require("assert");
 describe('class creation', () => {
 
   it('is as simple as `class XXX {}`', function() {
-    let TestClass;
+    class TestClass {};
 
     const instance = new TestClass();
     assert.equal(typeof instance, 'object');
   });
 
   it('class is block scoped', () => {
-    class Inside {}
     {class Inside {}}
     assert.equal(typeof Inside, 'undefined');
   });
 
   it('special method is `constructor`', function() {
     class User {
-      constructor(id) {}
+      constructor(id) { this.id = id; }
     }
 
     const user = new User(42);
@@ -29,6 +28,7 @@ describe('class creation', () => {
 
   it('defining a method is simple', function() {
     class User {
+        writesTests() { return false; }
     }
 
     const notATester = new User();
@@ -38,7 +38,7 @@ describe('class creation', () => {
   it('multiple methods need no commas (opposed to object notation)', function() {
     class User {
       wroteATest() { this.everWroteATest = true; }
-      isLazy() {  }
+      isLazy() { return !this.everWroteATest; }
     }
 
     const tester = new User();
@@ -48,7 +48,7 @@ describe('class creation', () => {
   });
 
   it('anonymous class', () => {
-    const classType = typeof {};
+    const classType = typeof class {};
     assert.equal(classType, 'function');
   });
 
