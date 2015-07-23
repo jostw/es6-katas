5019a424d9ac3f55858fa69cabaac0bd93747a49
jos <mail@jos.tw>
2015-07-23 10:25:22 +0800

    fix 24-class-static-keyword.js

--- tests/24-class-static-keyword.js
+++ tests/24-class-static-keyword.js
@@ -12,14 +12,14 @@ describe('inside a class you can use the `static` keyword', () => {
 
     it('a static method just has the prefix `static`', () => {
       class TestFactory {
-        makeTest() { return new UnitTest(); }
+        static makeTest() { return new UnitTest(); }
       }
 
       assert.ok(TestFactory.makeTest() instanceof UnitTest);
     });
 
     it('the method name can be dynamic/computed at runtime', () => {
-      const methodName = 'makeTest';
+      const methodName = 'createTest';
       class TestFactory {
         static [methodName]() { return new UnitTest(); }
       }
@@ -31,7 +31,7 @@ describe('inside a class you can use the `static` keyword', () => {
   describe('for accessors', () => {
     it('a getter name can be static, just prefix it with `static`', () => {
       class UnitTest {
-        get testType() { return 'unit'; }
+        static get testType() { return 'unit'; }
       }
 
       assert.equal(UnitTest.testType, 'unit');
@@ -40,7 +40,7 @@ describe('inside a class you can use the `static` keyword', () => {
     it('even a static getter name can be dynamic/computed at runtime', () => {
       const type = 'test' + 'Type';
       class IntegrationTest {
-        get type() { return 'integration'; }
+        static get [type]() { return 'integration'; }
       }
 
       assert.ok('testType' in IntegrationTest);
