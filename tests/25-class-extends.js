833e1823ce34650aa5193b62beb2071daa70254f
jos <mail@jos.tw>
2015-07-24 10:50:51 +0800

    fix 25-class-extends.js

--- tests/25-class-extends.js
+++ tests/25-class-extends.js
@@ -8,21 +8,21 @@ describe('classes can inherit from another', () => {
   describe('the default super class is Object', () => {
 
     it('class A is an instance of Object', () => {
-      let A;
+      class A {};
 
       assert.equal(new A() instanceof Object, true);
     });
 
     it('B extends A, B is also instance of Object', () => {
       class A {}
-      class B {}
+      class B extends A {}
 
       assert.equal(new B() instanceof A, true);
       assert.equal(new B() instanceof Object, true);
     });
 
     it('class can extend `null`, not an instance of Object', () => {
-      class NullClass extends Object {}
+      class NullClass extends null {}
 
       let nullInstance = new NullClass();
       assert.equal(nullInstance instanceof Object, false);
@@ -32,7 +32,7 @@ describe('classes can inherit from another', () => {
 
   describe('instance of', () => {
     it('when B inherits from A, `new B()` is also an instance of A', () => {
-      let A;
+      class A {};
       class B extends A {}
 
       assert.equal(new B() instanceof A, true);
@@ -40,9 +40,10 @@ describe('classes can inherit from another', () => {
 
     it('extend over multiple levels', () => {
       class A {}
+      class B extends A {}
       class C extends B {}
 
-      let instance = C;
+      let instance = new C();
       assert.equal(instance instanceof A, true);
     });
   });
