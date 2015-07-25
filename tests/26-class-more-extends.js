0db5e07545cfb0d675d27a1106dd75b4088c0624
jos <mail@jos.tw>
2015-07-25 11:44:09 +0800

    fix 26-class-more-extends.js

--- tests/26-class-more-extends.js
+++ tests/26-class-more-extends.js
@@ -6,7 +6,7 @@ var assert = require("assert");
 describe('class can inherit from another', () => {
 
   it('extend an `old style` "class", a function, still works', () => {
-    let A;
+    let A = () => {};
     class B extends A {}
 
     assert.equal(new B() instanceof A, true);
@@ -16,11 +16,11 @@ describe('class can inherit from another', () => {
     class A {}
     class B extends A {}
     it('A is the prototype of B', () => {
-      const isIt = A.isPrototypeOf(null);
+      const isIt = A.isPrototypeOf(B);
       assert.equal(isIt, true);
     });
     it('A`s prototype is also B`s prototype', () => {
-      const proto = B;
+      const proto = B.prototype;
       // Remember: don't touch the assert!!! :)
       assert.equal(A.prototype.isPrototypeOf(proto), true);
     });
@@ -29,14 +29,14 @@ describe('class can inherit from another', () => {
   describe('`extends` using an expression', () => {
     it('eg the inline assignment of the parent class', () => {
       let A;
-      class B extends (A = {}) {}
+      class B extends (A = class {}) {}
 
       assert.equal(new B() instanceof A, true);
     });
 
     it('or calling a function that returns the parent class', () => {
       const returnParent = (beNull) => beNull ? null : class {};
-      class B extends (returnParent) {}
+      class B extends (returnParent(true)) {}
 
       assert.equal(Object.getPrototypeOf(B.prototype), null);
     });
