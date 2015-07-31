3bdbac277338cb737c93ebe80010d816833e7073
jos <mail@jos.tw>
2015-07-31 14:03:42 +0800

    fix 32-array-find.js

--- tests/32-array-find.js
+++ tests/32-array-find.js
@@ -6,19 +6,19 @@ var assert = require("assert");
 describe('`Array.prototype.find` makes finding items in arrays easier', () => {
 
   it('takes a compare function', function() {
-    const found = [false, true].find(true);
+    const found = [false, true].find(item => item);
 
     assert.equal(found, true);
   });
 
   it('returns the first value found', function() {
-    const found = [0, 1].find(item => item > 1);
+    const found = [0, 1, 2].find(item => item > 1);
 
     assert.equal(found, 2);
   });
 
   it('returns `undefined` when nothing was found', function() {
-    const found = [1, 2, 3].find(item => item === 2);
+    const found = [1, 2, 3].find(item => item === 0);
 
     assert.equal(found, void 0);
   });
@@ -26,7 +26,7 @@ describe('`Array.prototype.find` makes finding items in arrays easier', () => {
   it('combined with destructuring complex compares become short', function() {
     const bob = {name: 'Bob'};
     const alice = {name: 'Alice'};
-    const found = [bob, alice].find(({name:{length}}) => length);
+    const found = [bob, alice].find(({name:{length}}) => length > 3);
 
     assert.equal(found, alice);
   });
