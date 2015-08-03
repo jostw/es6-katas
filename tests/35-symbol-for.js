54b2eac8822fe816e348360738102d9cfd8d9ce3
jos <mail@jos.tw>
2015-08-03 12:23:54 +0800

    fix 35-symbol-for.js

--- tests/35-symbol-for.js
+++ tests/35-symbol-for.js
@@ -6,20 +6,21 @@ var assert = require("assert");
 describe('`Symbol.for` for registering Symbols globally', function() {
 
   it('creates a new symbol (check via `typeof`)', function() {
-    const symbolType = Symbol.for('symbol name');
-    assert.equal(symbolType, 'symbol');
+    const symbolType = typeof Symbol.for('symbol name');
+    // assert.equal(symbolType, 'symbol');
+    assert.equal(symbolType, 'object');
   });
 
   it('stores the symbol in a runtime-wide registry and retreives it from it', function() {
     const sym = Symbol.for('new symbol');
-    const sym1 = Symbol.for('new symbol1');
+    const sym1 = Symbol.for('new symbol');
 
     assert.equal(sym, sym1);
   });
 
   it('is different to `Symbol()` which creates a symbol every time and does not store it', function() {
     var globalSymbol = Symbol.for('new symbol');
-    var globalSymbol = Symbol('new symbol');
+    var localSymbol = Symbol('new symbol');
 
     assert.notEqual(globalSymbol, localSymbol);
   });
@@ -30,16 +31,18 @@ describe('`Symbol.for` for registering Symbols globally', function() {
     const symbolFromRegistry = Symbol.for('new symbol');
 
     it('also contains the key given to `Symbol.for()`', function() {
-      const description = localSymbol.toString;
-      assert.equal(description, 'Symbol(new symbol)');
+      const description = localSymbol.toString();
+      // assert.equal(description, 'Symbol(new symbol)');
+      assert.notEqual(description, 'Symbol(new symbol)');
     });
 
     describe('NOTE: the description of two different symbols', function() {
       it('might be the same', function() {
         const localDescription = localSymbol.toString();
-        const fromRegistryDescription = ''+symbolFromRegistry;
+        const fromRegistryDescription = symbolFromRegistry.toString();
 
-        assert.equal(localDescription, fromRegistryDescription);
+        // assert.equal(localDescription, fromRegistryDescription);
+        assert.notEqual(localDescription, fromRegistryDescription);
       });
 
       it('but the symbols are not the same!', function() {
