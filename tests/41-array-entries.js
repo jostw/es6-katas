6b1024e7a2b7c36a3a0db572e3fbeb07e0d7e821
jos <mail@jos.tw>
2015-08-09 12:47:40 +0800

    fix 41-array-entries.js

--- tests/41-array-entries.js
+++ tests/41-array-entries.js
@@ -7,7 +7,7 @@ describe('`[].entries()` returns an iterator object with all entries', function(
 
   it('returns key+value for each element', function() {
     const arr = ['a', 'b', 'c'];
-    const entriesAsArray = Array.from(arr.___());
+    const entriesAsArray = Array.from(arr.entries());
 
     assert.deepEqual(entriesAsArray, [[0,"a"], [1,"b"], [2,"c"]]);
   });
@@ -15,7 +15,7 @@ describe('`[].entries()` returns an iterator object with all entries', function(
   it('empty elements contain the value `undefined`', function() {
     const arr = ['one'];
     arr[2] = 'three';
-    const secondValue = arr.entries();
+    const secondValue = Array.from(arr.entries())[1];
 
     assert.deepEqual(secondValue, [1, void 0]);
   });
@@ -23,8 +23,8 @@ describe('`[].entries()` returns an iterator object with all entries', function(
   describe('returns an iterable', function() {
 
     it('has `next()` to iterate', function() {
-      const arr = ['one'];
-      const value = arr;
+      const arr = ['one'].entries();
+      const value = arr.next().value;
 
       assert.deepEqual(value, [0, 'one']);
     });
