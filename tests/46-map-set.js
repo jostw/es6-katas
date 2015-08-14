c4d1bf9577d6c9e7b86e9a9aee14db1f5fb0c179
jos <mail@jos.tw>
2015-08-14 15:42:33 +0800

    fix 46-map-set.js

--- tests/46-map-set.js
+++ tests/46-map-set.js
@@ -7,7 +7,7 @@ describe('`Map.prototype.set` adds a new element with key and value to a Map', f
 
   it('simplest use case is `set(key, value)` and `get(key)`', function() {
     let map = new Map();
-    map.set();
+    map.set('key', 'value');
 
     assert.equal(map.get('key'), 'value');
   });
@@ -15,7 +15,7 @@ describe('`Map.prototype.set` adds a new element with key and value to a Map', f
   it('the key can be a complex type too', function() {
     const noop = function() {};
     let map = new Map();
-    map.set(function() {}, 'the real noop');
+    map.set(noop, 'the real noop');
 
     assert.equal(map.get(noop), 'the real noop');
   });
@@ -23,7 +23,7 @@ describe('`Map.prototype.set` adds a new element with key and value to a Map', f
   it('calling `set()` again with the same key replaces the value', function() {
     let map = new Map();
     map.set('key', 'value');
-    map.set('key', 'value3');
+    map.set('key', 'value1');
 
     assert.equal(map.get('key'), 'value1');
   });
@@ -31,7 +31,8 @@ describe('`Map.prototype.set` adds a new element with key and value to a Map', f
   it('`set()` returns the map object, it`s chainable', function() {
     let map = new Map();
     map.set(1, 'one')
-       .set(2, 'two');
+       .set(2, 'two')
+       .set(3, 'three');
 
     assert.deepEqual([...map.keys()], [1,2,3]);
     assert.deepEqual([...map.values()], ['one', 'two', 'three']);
