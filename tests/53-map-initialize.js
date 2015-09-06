4f44dabacc647248bdca600d09d6953e44cf96ad
jos <mail@jos.tw>
2015-09-06 23:29:15 +0800

    fix 53-map-initialize.js

--- tests/53-map-initialize.js
+++ tests/53-map-initialize.js
@@ -6,18 +6,18 @@ var assert = require("assert");
 describe('initialize a `Map`', function(){
 
   it('a `new Map()` is empty, has size=0', function() {
-    const mapSize = new Map();
+    const mapSize = new Map().size;
     assert.equal(mapSize, 0);
   });
 
   it('init Map with `[[]]` has a size=1', function() {
-    const mapSize = new Map().size;
+    const mapSize = new Map([[]]).size;
 
     assert.equal(mapSize, 1);
   });
 
   it('init a Map with `[[1]]` is the same as `map.set(1, void 0)`', function() {
-    let map1 = new Map();
+    let map1 = new Map([[1]]);
     let map2 = new Map().set(1, void 0);
 
     assertMapsEqual(map1, map2);
@@ -27,7 +27,7 @@ describe('initialize a `Map`', function(){
     const pair1 = [1, 'one'];
     const pair2 = [2, 'two'];
 
-    const map = new Map();
+    const map = new Map([pair1, pair2]);
 
     assertMapsEqual(map, new Map().set(...pair1).set(...pair2));
   });
@@ -38,7 +38,7 @@ describe('initialize a `Map`', function(){
     const pair3 = [1, 'eins'];
     const pair4 = [2, 'two'];
 
-    const map = new Map([pair3, pair1, pair2, pair4]);
+    const map = new Map([pair1, pair2, pair3, pair4]);
 
     assertMapsEqual(map, new Map().set(...pair3).set(...pair4));
   });
@@ -47,7 +47,7 @@ describe('initialize a `Map`', function(){
     let map = new Map();
     const obj = {x: 1, y: 2};
     const keys = Object.keys(obj);
-    keys.forEach(key => map.set());
+    keys.forEach(key => map.set(key, obj[key]));
 
     const expectedEntries = [['x', 1], ['y', 2]];
     assertMapsEqual(map, expectedEntries);
