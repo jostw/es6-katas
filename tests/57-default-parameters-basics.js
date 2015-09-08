fb6c98b873c431b4949dc5e0395a501cafebc8fe
jos <mail@jos.tw>
2015-09-08 12:32:16 +0800

    fix 57-default-parameters-basics.js

--- tests/57-default-parameters-basics.js
+++ tests/57-default-parameters-basics.js
@@ -6,20 +6,20 @@ var assert = require("assert");
 describe('default parameters make function parameters more flexible', () => {
 
   it('define it using an assignment to the parameter `function(param=1){}`', function() {
-    let number = (int) => int;
+    let number = (int = 0) => int;
 
     assert.equal(number(), 0);
   });
 
   it('it is used when undefined is passed', function() {
     let number = (int = 23) => int;
-    const param = 42;
+    const param = undefined;
 
     assert.equal(number(param), 23);
   });
 
   it('it is not used when a value is given', function() {
-    function xhr() {
+    function xhr(method) {
       return method;
     }
 
@@ -27,7 +27,7 @@ describe('default parameters make function parameters more flexible', () => {
   });
 
   it('it is evaluated at run time', function() {
-    let defaultValue;
+    let defaultValue = 42;
     function xhr(method = `value: ${defaultValue}`) {
       return method;
     }
@@ -37,7 +37,7 @@ describe('default parameters make function parameters more flexible', () => {
   });
 
   it('it can also be a function', function() {
-    let defaultValue;
+    let defaultValue = () => {};
     function fn(value = defaultValue()) {
       return value;
     }
