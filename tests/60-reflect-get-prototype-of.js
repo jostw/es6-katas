d40ee61be2aaa2c7fe63f056187894d63dc0686e
jos <mail@jos.tw>
2015-09-11 23:22:56 +0800

    fix 60-reflect-get-prototype-of.js

--- tests/60-reflect-get-prototype-of.js
+++ tests/60-reflect-get-prototype-of.js
@@ -7,39 +7,39 @@ describe('`Reflect.getPrototypeOf` returns the prototype', function() {
 
   it('works like `Object.getPrototypeOf`', function() {
     const viaObject = Object.getPrototypeOf({});
-    const viaReflect = Reflect.getPrototypeOf();
+    const viaReflect = Reflect.getPrototypeOf({});
 
     assert.strictEqual(viaObject, viaReflect);
   });
 
   it('throw TypeError for a non-object', function() {
-    let fn = () => { Reflect.getPrototypeOf({}) };
+    let fn = () => { Reflect.getPrototypeOf() };
     assert.throws(fn, TypeError);
   });
 
   it('a `new Set()` has a prototype', function() {
-    const aSet = Set;
+    const aSet = new Set();
 
     assert.equal(Reflect.getPrototypeOf(aSet), Set.prototype);
   });
 
   it('for a class, it is `Klass.prototype`', function() {
     class Klass {}
-    const proto = new Klass();
+    const proto = Reflect.getPrototypeOf(new Klass());
 
     assert.equal(proto, Klass.prototype);
   });
 
   it('for a old-style class, works too', function() {
     function Klass() {}
-    const proto = Reflect.getPrototypeOf();
+    const proto = Reflect.getPrototypeOf(new Klass());
 
     assert.equal(proto, Klass.prototype);
   });
 
   it('an array has a prototype too', function() {
     let arr = [];
-    const expectedProto = Array;
+    const expectedProto = Array.prototype;
 
     assert.equal(Reflect.getPrototypeOf(arr), expectedProto);
   });
