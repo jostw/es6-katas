d46b6eb064fae1cd22f8e297f93d94d46b6a8354
jos <mail@jos.tw>
2015-10-06 16:26:07 +0800

    fix 71-string-repeat.js

--- tests/71-string-repeat.js
+++ tests/71-string-repeat.js
@@ -7,42 +7,42 @@ describe('`str.repeat(x)` appends `x` copies of `str` to each other and returns 
 
   describe('pass the count to `str.repeat(count)`', function() {
     it('for `1` the string stays the same', function() {
-      const what = '???'.repeat();
+      const what = 'one'.repeat(1);
       assert.equal(what, 'one');
     });
     it('for `3` the string `x` becomes `xxx`', function() {
-      const actual = 'x'.repeets;
+      const actual = 'x'.repeat(3);
       assert.equal(actual, 'xxx');
     });
     it('for `0` an empty string is returned', function() {
-      const dontRepeat = 1;
+      const dontRepeat = 0;
       assert.equal('shrink'.repeat(dontRepeat), '');
     });
 
     it('the count is not an int, such as "3", it gets coerced to an int', function() {
-      const repeated = ''.repeat('2');
+      const repeated = 'three'.repeat('3');
       assert.equal(repeated, 'threethreethree');
     });
   });
 
   describe('throws an error for', function() {
     it('a count of <0', function() {
-      const belowZero = 1;
+      const belowZero = -1;
       assert.throws(() => { ''.repeat(belowZero); }, RangeError);
     });
     it('a count of +Infinty', function() {
-      let infinity = 'infinity';
+      let infinity = '+Infinity';
       assert.throws(() => { ''.repeat(infinity); }, RangeError);
     });
   });
 
   describe('accepts everything that can be coerced to a string', function() {
     it('e.g. a boolean', function() {
-      let aBool = true;
+      let aBool = false;
       assert.equal(String.prototype.repeat.call(aBool, 2), 'falsefalse');
     });
     it('e.g. a number', function() {
-      let aNumber;
+      let aNumber = 1;
       assert.equal(String.prototype.repeat.call(aNumber, 2), '11');
     });
   });
@@ -51,7 +51,7 @@ describe('`str.repeat(x)` appends `x` copies of `str` to each other and returns 
     it('calls `toString()` to make it a string', function() {
       class MyString { toString() { return 'my string'; } }
 
-      const expectedString = '';
+      const expectedString = 'my string';
 
       assert.equal(String(new MyString()).repeat(1), expectedString);
     });
@@ -63,7 +63,7 @@ describe('`str.repeat(x)` appends `x` copies of `str` to each other and returns 
         }
       }
 
-      let repeated = new X().repeat(2);
+      let repeated = String(new X()).repeat(2);
 
       assert.equal(repeated, '11');
     });
